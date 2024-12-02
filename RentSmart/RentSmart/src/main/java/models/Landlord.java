bbe0c992ccf6b9900e8fa28b6d59eee26ee534f7
bmathew05 <112408795+bmathew05@users.noreply.github.com>
2024-12-01 19:15:37 -0800

    Delete RentSmart directory

--- RentSmart/RentSmart/src/main/java/models/Landlord.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class Landlord extends User {
-    public Landlord(String username, String password) {
-        super(username, password, "landlord");
-    }
-}
--- RentSmart/RentSmart/src/main/java/models/Tenant.java
+++
@@ -1,7 +0,0 @@
-package models;
-
-public class Tenant extends User {
-    public Tenant(String username, String password) {
-        super(username, password, "tenant");
-    }
-}
